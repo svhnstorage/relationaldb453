6ed300cf20297da3e849cb18615cd14cad54171b
svhnstorage <svhnstorage@gmail.com>
2021-04-25 20:41:19 -0400

    temp final css

--- client/src/NavBar.js
+++ client/src/NavBar.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import './App.css';
+
 
 function NavBar(){
 	return(
-		<ul>
+		
+		<ul style={
+			{
+				display: "flex",
+				flexDirection: "row",
+				justifyContent: "space-around",
+				listStyleType: "none",
+				fontSize: "30px",
+				backgroundColor: "#FFA500",
+				textDecoration: "none",
+			}
+
+		}>
 			<li><Link to ='/'> Home </Link></li>
 			<li><Link to ='/assets'> Assets </Link></li>
 			<li><Link to ='/students'> Students </Link></li>
 			<li><Link to ='/employees'> Employees </Link></li>
 			<li><Link to ='/loans'> Loans </Link></li>
 		</ul>
+		
 	);
 }
 
