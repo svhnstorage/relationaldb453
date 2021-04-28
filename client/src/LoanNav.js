bbf7435f8877908cce30581d8c2155cae8fd1f29
svhnstorage <svhnstorage@gmail.com>
2021-04-28 13:33:24 -0400

    nav bar styled

--- client/src/LoanNav.js
+++ client/src/LoanNav.js
@@ -4,22 +4,62 @@ import './App.css';
 
 
 function LoanNav(){
+
+	function changeBackground(e) {
+    	e.target.style.backgroundColor = '#ddd';
+    	e.target.style.color = "black";
+  	}
+
+  	function changeBackgroundBack(e) {
+    	e.target.style.backgroundColor = '#333';
+    	e.target.style.color = "#f2f2f2";
+  	}
+
 	return(
 		
 		<ul style={
 			{
 				display: "flex",
 				flexDirection: "row",
-				justifyContent: "space-around",
+				justifyContent: "flex-start",
 				listStyleType: "none",
 				fontSize: "30px",
-				backgroundColor: "#FFA500",
-				textDecoration: "none",
+				backgroundColor: "#333",
+				padding: "5px 2px"
 			}
-
 		}>
-			<li><Link to ='/studentloan'> Students </Link></li>
-			<li><Link to ='/employeeloan'> Employees </Link></li>
+			<li style={
+				{
+					padding: "10px 16px"
+				}
+			}><Link to ='/studentloan' 
+				style={
+					{
+						padding: "14px 16px",
+						textDecoration: "none",
+						color: "#f2f2f2",
+						fontFamily: "Arial, Helvetica, sans-serif"
+					}
+				}
+				onMouseOver={changeBackground}
+				onMouseLeave={changeBackgroundBack}
+			> Students </Link></li>
+			<li style={
+				{
+					padding: "10px 16px"
+				}
+			}><Link to ='/employeeloan' 
+				style={
+					{
+						padding: "14px 16px",
+						textDecoration: "none",
+						color: "#f2f2f2",
+						fontFamily: "Arial, Helvetica, sans-serif"
+					}
+				}
+				onMouseOver={changeBackground}
+				onMouseLeave={changeBackgroundBack}
+			> Employees </Link></li>
 		</ul>
 		
 	);
--- client/src/NavBar.js
+++ client/src/NavBar.js
@@ -4,25 +4,111 @@ import './App.css';
 
 
 function NavBar(){
+
+	function changeBackground(e) {
+    	e.target.style.backgroundColor = '#ddd';
+    	e.target.style.color = "black";
+  	}
+
+  	function changeBackgroundBack(e) {
+    	e.target.style.backgroundColor = '#333';
+    	e.target.style.color = "#f2f2f2";
+  	}
+
+
 	return(
 		
 		<ul style={
 			{
 				display: "flex",
 				flexDirection: "row",
-				justifyContent: "space-around",
+				justifyContent: "flex-start",
 				listStyleType: "none",
 				fontSize: "30px",
-				backgroundColor: "#FFA500",
-				textDecoration: "none",
+				backgroundColor: "#333",
+				padding: "5px 2px"
 			}
-
 		}>
-			<li><Link to ='/'> Home </Link></li>
-			<li><Link to ='/assets'> Assets </Link></li>
-			<li><Link to ='/students'> Students </Link></li>
-			<li><Link to ='/employees'> Employees </Link></li>
-			<li><Link to ='/loans'> Loans </Link></li>
+			<li style={
+				{
+					padding: "10px 16px"
+				}
+			}><Link to ='/'
+				style={
+					{
+						padding: "14px 16px",
+						textDecoration: "none",
+						color: "#f2f2f2",
+						fontFamily: "Arial, Helvetica, sans-serif"
+					}
+				}
+				onMouseOver={changeBackground}
+				onMouseLeave={changeBackgroundBack}
+			> Home </Link></li>
+			<li style={
+				{
+					padding: "10px 16px"
+				}
+			}><Link to ='/assets'
+				style={
+					{
+						padding: "14px 16px",
+						textDecoration: "none",
+						color: "#f2f2f2",
+						fontFamily: "Arial, Helvetica, sans-serif"
+					}
+				}
+				onMouseOver={changeBackground}
+				onMouseLeave={changeBackgroundBack}
+			> Assets </Link></li>
+			<li style={
+				{
+					padding: "10px 16px"
+				}
+			}><Link to ='/students' 
+				style={
+					{
+						padding: "14px 16px",
+						textDecoration: "none",
+						color: "#f2f2f2",
+						fontFamily: "Arial, Helvetica, sans-serif"
+					}
+				}
+				onMouseOver={changeBackground}
+				onMouseLeave={changeBackgroundBack}
+			> Students </Link></li>
+			<li style={
+				{
+					padding: "10px 16px"
+				}
+			}><Link to ='/employees' 
+				style={
+					{
+						padding: "14px 16px",
+						textDecoration: "none",
+						color: "#f2f2f2",
+						fontFamily: "Arial, Helvetica, sans-serif"
+					}
+				}
+				onMouseOver={changeBackground}
+				onMouseLeave={changeBackgroundBack}
+			> Employees </Link></li>
+			<li style={
+				{
+					padding: "10px 16px"
+				}
+			}><Link to ='/loans' 
+				style={
+					{
+						padding: "14px 16px",
+						textDecoration: "none",
+						color: "#f2f2f2",
+						fontFamily: "Arial, Helvetica, sans-serif"
+					}
+				}
+				onMouseOver={changeBackground}
+				onMouseLeave={changeBackgroundBack}
+			> Loans </Link></li>
 		</ul>
 		
 	);
